8fc99d157efa9edbd9a7e0edd4a548515ec5b265
DrGear <79474598+DrGear@users.noreply.github.com>
2025-01-25 01:52:20 -0400

    remove app mention

--- plugin.py
+++ plugin.py
@@ -15,13 +15,9 @@ def calculate_time(epochs, seconds_per_epoch):
 
 def applio_plugin():
     gr.Markdown(value="Calculate the total amount of time for a training session.")
-    with gr.Blocks() as app:
-        with gr.Column():
-            epochs_input = gr.Number(label="Number of epochs.")
-            seconds_input = gr.Number(label="Seconds per epoch.")
-            calculate_button = gr.Button("Calculate time remaining")
-            remaining_time_output = gr.Textbox(label="Remaining time", interactive=False)
-
-            calculate_button.click(calculate_time, inputs=[epochs_input, seconds_input], outputs=[remaining_time_output])
-
-
+    with gr.Column():
+        epochs_input = gr.Number(label="Number of epochs.")
+        seconds_input = gr.Number(label="Seconds per epoch.")
+        calculate_button = gr.Button("Calculate time remaining")
+        remaining_time_output = gr.Textbox(label="Remaining time", interactive=False)
+        calculate_button.click(calculate_time, inputs=[epochs_input, seconds_input], outputs=[remaining_time_output])
